21a28807a0e1f71b344c6164acc085fdf4b5900a
Antoine Toisoul <antoine.toisoul13@imperial.ac.uk>
2016-09-01 11:28:30 +0100

    LICENSE and comments added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,42 @@
+/*
+ *     PFM_ReadWrite
+ *
+ *     Authors:  Antoine TOISOUL LE CANN
+ *
+ *     Copyright © 2016 Antoine TOISOUL
+ *              All rights reserved
+ *
+ *
+ * PFM_ReadWrite is free software: you can redistribute it and/or modify
+ *
+ * it under the terms of the GNU Lesser General Public License as published by
+ *
+ * the Free Software Foundation, either version 3 of the License, or
+ *
+ * (at your option) any later version.
+ *
+ * PFM_ReadWrite is distributed in the hope that it will be useful,
+ *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file main.cpp
+ * \brief Example of how to read and save PFM files.
+ * \author Antoine Toisoul Le Cann
+ * \date September, 1st, 2016
+ *
+ * Example of how to read and save PFM files.
+ */
+
 #include <iostream>
 #include <string>
 
@@ -16,7 +55,6 @@ int main(void)
     waitKey(0);
 
     savePFM(image, "image_saved.pfm");
-
 
     return 0;
 }
